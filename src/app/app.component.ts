0c702e7261a22143054c5f55fb37fc1935afb2ee
Adrien Keller <adrien.keller@epitech.eu>
2021-11-13 18:10:22 +0100

    Init service on module init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TravelService} from "./travel.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Travel Book';
+
+  constructor(private readonly travelService: TravelService) {
+  }
+  
+  public ngOnInit(): void {
+    this.travelService.init();
+  }
 }
